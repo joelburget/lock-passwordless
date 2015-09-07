c27d00dcdf401803a78af85c5d0dd43365960259
Gabriel Andretta <gabriel.andretta@gmail.com>
2015-09-07 20:50:19 -0300

    Another POC for the sliding animation

--- src/lock/cred_pane.jsx
+++ src/lock/cred_pane.jsx
@@ -9,28 +9,9 @@ import Terms from '../lock/terms';
 const ReactTransitionGroup = React.addons.TransitionGroup;
 const ReactCSSTransitionGroup = React.addons.CSSTransitionGroup;
 
-let height;
+let prevHeight;
 
 export default class CredPane extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {placeholderHeight: height};
-  }
-
-  componentDidMount() {
-    setTimeout(() => {
-      this.setState({placeholderHeight: undefined});
-    }, 850);
-  }
-
-  componentDidUpdate() {
-    if (!this.state.placeholderHeight) {
-      const node = React.findDOMNode(this.refs.container);
-      var computedStyle = window.getComputedStyle(node, null);
-      height = computedStyle.height;
-    }
-  }
-
   render() {
     const { auxiliaryPane, className, lock } = this.props;
 
@@ -48,29 +29,60 @@ export default class CredPane extends React.Component {
       name = "";
     }
 
-    const container = (
-      <div ref="container">
-        <ReactTransitionGroup>
-          {globalError && <GlobalError key="global-error" message={globalError} />}
-        </ReactTransitionGroup>
-        <div className="auth0-lock-content">
-          {this.props.children}
-        </div>
-        {l.ui.terms(lock) && <Terms content={l.ui.terms(lock)} />}
-      </div>
-    );
-
-    const placeholder = <div style={{height: this.state.placeholderHeight}} />;
-
     return (
       <div className={className + " auth0-lock-cred-pane"}>
         <Header name={name} backgroundUrl={backgroundUrl} logoUrl={icon}/>
-        {this.state.placeholderHeight ? placeholder : container}
+        <div ref="container">
+          <ReactTransitionGroup>
+            <Placeholder>
+              <ReactTransitionGroup>
+                {globalError && <GlobalError key="global-error" message={globalError} />}
+              </ReactTransitionGroup>
+              <div className="auth0-lock-content">
+                {this.props.children}
+              </div>
+              {l.ui.terms(lock) && <Terms content={l.ui.terms(lock)} />}
+            </Placeholder>
+          </ReactTransitionGroup>
+        </div>
         <SubmitButton disabled={disableSubmit} />
         <ReactCSSTransitionGroup transitionName="slide">
           {auxiliaryPane}
         </ReactCSSTransitionGroup>
       </div>
     );
+  }
+}
+
+class Placeholder extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {show: true};
+  }
+
+  render() {
+    return <div>{this.state.show && this.props.children}</div>;
+  }
+
+  componentWillAppear(callback) {
+    const node = React.findDOMNode(this);
+    var computedStyle = window.getComputedStyle(node, null);
+    var height = computedStyle.height;
+    if (!prevHeight) {
+      prevHeight = height;
+      return
+    }
+
+    var paddingTop = computedStyle.paddingTop;
+    var paddingBottom = computedStyle.paddingBottom;
+    node.style.height = prevHeight;
+    setTimeout(function() {
+      node.style.transition = "all 0.4s 0.8s";
+      node.style.height = height;
+      prevHeight = height;
+    }, 17);
+    this.setState({show: false});
+    setTimeout(() => this.setState({show: true}), 800);
+    callback();
   }
 }
