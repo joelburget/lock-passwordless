bb3299f7fe5a482238d8bbaaf8e26ac7303cce97
Gabriel Andretta <gabriel.andretta@gmail.com>
2015-09-07 19:29:18 -0300

    POC for the sliding animation

--- src/lock/cred_pane.jsx
+++ src/lock/cred_pane.jsx
@@ -9,7 +9,28 @@ import Terms from '../lock/terms';
 const ReactTransitionGroup = React.addons.TransitionGroup;
 const ReactCSSTransitionGroup = React.addons.CSSTransitionGroup;
 
+let height;
+
 export default class CredPane extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {placeholderHeight: height};
+  }
+
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({placeholderHeight: undefined});
+    }, 850);
+  }
+
+  componentDidUpdate() {
+    if (!this.state.placeholderHeight) {
+      const node = React.findDOMNode(this.refs.container);
+      var computedStyle = window.getComputedStyle(node, null);
+      height = computedStyle.height;
+    }
+  }
+
   render() {
     const { auxiliaryPane, className, lock } = this.props;
 
@@ -27,9 +48,8 @@ export default class CredPane extends React.Component {
       name = "";
     }
 
-    return (
-      <div className={className + " auth0-lock-cred-pane"}>
-        <Header name={name} backgroundUrl={backgroundUrl} logoUrl={icon}/>
+    const container = (
+      <div ref="container">
         <ReactTransitionGroup>
           {globalError && <GlobalError key="global-error" message={globalError} />}
         </ReactTransitionGroup>
@@ -37,6 +57,15 @@ export default class CredPane extends React.Component {
           {this.props.children}
         </div>
         {l.ui.terms(lock) && <Terms content={l.ui.terms(lock)} />}
+      </div>
+    );
+
+    const placeholder = <div style={{height: this.state.placeholderHeight}} />;
+
+    return (
+      <div className={className + " auth0-lock-cred-pane"}>
+        <Header name={name} backgroundUrl={backgroundUrl} logoUrl={icon}/>
+        {this.state.placeholderHeight ? placeholder : container}
         <SubmitButton disabled={disableSubmit} />
         <ReactCSSTransitionGroup transitionName="slide">
           {auxiliaryPane}
